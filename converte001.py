a6e19f048da1c02082a6d946ea5cd695d4ff6bdb
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-28 06:06:03 -0300

    Update converte001.py

--- converte001.py
+++ converte001.py
@@ -8,7 +8,7 @@ def time_to_percentage(time):
     percentage = (total_minutes / (24 * 60)) * 100
     return round(percentage, 2)
 
-def calculate_percentage():
+def calculate_percentage(event=None):  # adicionado parâmetro event
     time = time_entry.get()
     if time:
         percentage = time_to_percentage(time)
@@ -16,10 +16,10 @@ def calculate_percentage():
 
 root = tk.Tk()
 root.title("Calculadora de Porcentagem do Dia")
-root.geometry('600x200')
+root.geometry('600x300')
 
 style = ttk.Style()
-style.configure("TButton", font=("Arial", 12), background="#1E90FF", foreground="black")
+style.configure("TButton", font=("Arial", 12), background="#1E90FF", foreground="grey")
 style.configure("TLabel", font=("Arial", 12), background="#F0F8FF")
 style.configure("TEntry", font=("Arial", 12))
 style.configure(".", background="#F0F8FF")
@@ -32,6 +32,7 @@ time_label.pack()
 
 time_entry = ttk.Entry(frame)
 time_entry.pack(pady=10)
+time_entry.bind('<Return>', calculate_percentage)  # novo comando adicionado aqui
 
 calculate_button = ttk.Button(frame, text="Calcular", command=calculate_percentage)
 calculate_button.pack(pady=10)
